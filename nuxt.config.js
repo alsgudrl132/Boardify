7930cc586d5b2a17e8f511ae53d40344d2d1c932
alsgudrl133 <alsgudrl133@ebsap.co.kr>
2024-10-29 17:11:13 +0900

    vercel 설정

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,8 +43,13 @@ export default {
     },
   },
 
+  target: "server",
   env: {
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  },
+
+  server: {
+    port: process.env.NODE_ENV === "production" ? null : 5000,
   },
 };
